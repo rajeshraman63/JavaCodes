71ab57f14a7771a0e1a2fc63126e50c8d763e85b
Rajesh Raman Bosak <rajeshraman63@gmail.com>
2017-12-23 06:06:48 -0800

    Create CharString.java

---
+++ CharString.java
@@ -0,0 +1,7 @@
+class CharString{
+    public static void main(String ... args){   
+        char[] charArray = new char[]{'a','r','r','a','y'};
+        String str = new String(charArray);
+        System.out.println(str);
+    }
+}
